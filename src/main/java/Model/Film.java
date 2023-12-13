5b80ddf005afc2cae3ae5685307287a2f1a161b3
Andrew <dyboschit@gmail.com>
2023-12-13 14:12:05 +0300

    External api use.

---
+++ src/main/java/Model/Film.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class Film {
+    private String title;
+    private String original_title;
+    private String original_title_romanised;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOriginal_title() {
+        return original_title;
+    }
+
+    public String getOriginal_title_romanised() {
+        return original_title_romanised;
+    }
+}
---
+++ src/main/java/Service/FilmService.java
@@ -0,0 +1,21 @@
+package Service;
+
+import Model.Film;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class FilmService {
+
+    @Autowired
+    private RestTemplate template = new RestTemplate();
+
+    public Object[] findAllFilmsComplete () {
+        return template.getForObject("https://ghibliapi.vercel.app/films/", Object[].class);
+    }
+
+    public Film[] findAllFilms () {
+        return template.getForObject("https://ghibliapi.vercel.app/films/", Film[].class);
+    }
+}
